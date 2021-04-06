b983a8faaff891bc547106e94dc8d877ac9c45be
misbiheyv <57451566+misbiheyv@users.noreply.github.com>
2021-04-06 20:47:15 +1000

    Add files via upload

---
+++ NotesDZ/NotesDZ/EditPage.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NotesDZ
+{
+    static class Instance
+    {
+        static public ObservableCollection<Notes> marks1 = new ObservableCollection<Notes>();
+        static public ObservableCollection<Notes> marks2 = new ObservableCollection<Notes>();
+        static public List<Notes> pool = new List<Notes>();
+        static public double l1 = 0, l2 = 0;
+        static public int currentId = 0;
+        static public bool flag = true;
+    }
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class EditPage : ContentPage
+    {
+        public EditPage()
+        {
+            InitializeComponent();
+
+            //подгружаю текст с уже существующей заметки
+            if(Instance.currentId != 0)
+                TextBox.Text = Instance.pool.Find(x => x.Id == Instance.currentId).Text;
+        }
+        private void TextBox_Completed(object sender, EventArgs e)
+        {
+            var text = TextBox.Text;
+            text.Normalize();
+
+            if (text != null && text.Length != 0)
+            {
+                if (Instance.currentId == 0)
+                {
+                    // создаю новый элемент
+
+                    Notes newNote = new Notes { Text = text, numLines = text.Split('\n').Length, Date = DateTime.Now.ToString() };
+                    Instance.currentId = newNote.Id;
+                    Instance.pool.Add(newNote);
+                }
+                else
+                {
+                    // редачу новый или уже существующий
+                    Instance.pool.Find(x => x.Id == Instance.currentId).Text = text;
+                    Instance.pool.Find(x => x.Id == Instance.currentId).numLines = text.Split('\n').Length;
+                    Instance.pool.Find(x => x.Id == Instance.currentId).Date = DateTime.Now.ToString();
+                }
+
+            }
+        }
+
+    }
+}
---
+++ NotesDZ/NotesDZ/MainPage.xaml.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace NotesDZ
+{
+    public class Notes
+    {
+        public string Text { get; set; }
+        public int numLines = 0;
+        public string Date { get; set; }
+        public int Id { get; private set; }
+        private static int globalId = 0;
+
+        public Notes()
+        {
+            Id = ++globalId;
+        }
+    }
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindableLayout.SetItemsSource(Col1, Instance.marks1);
+            BindableLayout.SetItemsSource(Col2, Instance.marks2);
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var button = sender as Button;
+            button.IsEnabled = false;
+            Instance.currentId = 0;
+
+            var form = new EditPage();
+            Navigation.PushAsync(form);
+            form.Disappearing += (send, ev) =>
+            {
+                button.IsEnabled = true;
+
+                //ребилд сцены
+
+                rebuild();
+            };
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            var smth = sender as Frame;
+            Instance.currentId = int.Parse(smth.AutomationId.ToString());
+
+            var form = new EditPage();
+            Navigation.PushAsync(form);
+
+            form.Disappearing += (send, ev) =>
+            {
+                //ребилд сцены
+
+                rebuild();
+            };
+        }
+
+        private void rebuild()
+        {
+            Instance.l1 = 0;
+            Instance.l2 = 0;
+            Instance.marks1.Clear();
+            Instance.marks2.Clear();
+            for (int i = 0; i < Instance.pool.Count; i++)
+            {
+                if (Instance.l1 <= Instance.l2)
+                {
+                    Instance.marks1.Add(Instance.pool[i]);
+                    Instance.l1 += Instance.pool[i].numLines + 2;
+                }
+                else
+                {
+                    Instance.marks2.Add(Instance.pool[i]);
+                    Instance.l2 += Instance.pool[i].numLines + 2;
+                }
+            }
+        }
+        private async void PanGestureRecognizer_PanUpdatedLeft(object sender, PanUpdatedEventArgs e)
+        {
+
+            var thisFrame = sender as Frame;
+
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+                    {
+                        //thisFrame.TranslationX = -Math.Abs(e.TotalX);
+                        thisFrame.TranslationX = Math.Min(0, e.TotalX * 2);
+                        //Math.Max(Math.Min(0, e.TotalX), -Math.Abs(Content.Width - 700));
+                        if (Instance.flag && Math.Abs(thisFrame.TranslationX / (MainStack.Width / 6)) > 0.4)
+                        {
+                            Instance.flag = false;
+
+                            if (await DisplayAlert("Delete", "Are you sure?", "Yes", "Cancel"))
+                            {
+                                
+                                Instance.pool.Remove(Instance.pool.Find(x => x.Id == int.Parse(thisFrame.AutomationId)));
+
+                                //ребилд сцены
+
+                                rebuild();
+
+                                Instance.flag = true;
+                            }
+                            else
+                            {
+                                thisFrame.TranslationX = 0;
+                                Instance.flag = true;
+                            }
+                        }
+
+                        break;
+                    }
+
+
+                    case GestureStatus.Completed:
+                    {
+                        thisFrame.TranslationX = 0;
+
+                        break;
+                    }
+            }
+
+
+        }
+
+        private async void PanGestureRecognizer_PanUpdatedRight(object sender, PanUpdatedEventArgs e)
+        {
+
+            var thisFrame = sender as Frame;
+
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+                    {
+                        //thisFrame.TranslationX = Math.Abs(e.TotalX);
+                        thisFrame.TranslationX = Math.Max(0, e.TotalX * 2);
+                        //Math.Max(Math.Min(0, e.TotalX), -Math.Abs(Content.Width - 700));
+                        if (Instance.flag && Math.Abs(thisFrame.TranslationX / (MainStack.Width / 6)) > 0.4)
+                        {
+                            Instance.flag = false;
+
+                            if (await DisplayAlert("Delete", "Are you sure?", "Yes", "Cancel"))
+                            {
+                                Instance.pool.Remove(Instance.pool.Find(x => x.Id == int.Parse(thisFrame.AutomationId)));
+
+                                //ребилд сцены
+
+                                rebuild();
+
+                                Instance.flag = true;
+                            }
+                            else
+                            {
+                                thisFrame.TranslationX = 0;
+                                Instance.flag = true;
+                            }
+                        }
+
+                        break;
+                    }
+
+
+                    case GestureStatus.Completed:
+                    {
+                        thisFrame.TranslationX = 0;
+
+                        break;
+                    }
+            }
+        }
+    }
+}
